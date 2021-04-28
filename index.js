9da3d7dda12395034c4df5b0d23c1d9189d8525a
jonahtabb <jonahtabb@gmail.com>
2021-04-28 12:07:05 -0400

    Added fade effect to the removal of the card on the dom

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ const citySelectorField = document.getElementById("select-city");
 stateSelectorField.disabled = true;
 citySelectorField.disabled = true;
 
-let removeCardButtons = [];
-
-let selectedStateName = "" ;
+let removeCardButtons = []; //Current list of cards displayed on the dom (Specifically, each card's 'remove card' button element)
+let selectedStateName = "" ; //Currently selected State name
 
 citySelectorField.onchange = getCityData;
-stateSelectorField.onchange = getCurrentState ;  //Don't use () because we want reference the function but not immediately call it when it initially gets read.
+stateSelectorField.onchange = getCurrentState ;  //Note to self: don't use () for 'onchange' because we want reference the function but not immediately call it when it initially gets read. These type of functions pass event info into the function.
 
-
+//Get the supported states on page load!
 getSupportedStates()
 
 async function getSupportedStates () {
@@ -26,13 +25,9 @@ async function getSupportedStates () {
     let resData = await resStates.json();
     let supportedStates = await resData.data;
     addStatesToList(supportedStates);
-    // console.log(supportedStates[0].state);
-    // await getSupportedCities(supportedStates[0].state);
-    // selectedStateName = supportedStates[0].state
-
 }
 
-//Ads supported states to drop down list
+//Adds supported states to drop down list
 function addStatesToList(arr) {
     let stateOptions = `<option>Choose...</option>`;
     arr.forEach(element => {
@@ -42,11 +37,12 @@ function addStatesToList(arr) {
     stateSelectorField.disabled = false;
 }
 
-
 //When stateSelectorField is changed, this function is run.
 function getCurrentState(e) {
     selectedStateName = e.target.value;
+    if(selectedStateName !== "Choose..."){
     getSupportedCities(selectedStateName);
+    }
     
 }
 
@@ -58,12 +54,11 @@ async function getSupportedCities(state) {
     addCitiesToList(supportedCities);
 }
 
-//Ads supported cities to drop down list
+//Adds supported cities to drop down list
 function addCitiesToList(cities) {
     let cityOptions = `<option>Choose...</option>`;
     cities.forEach(element => {
         cityOptions += `<option>${element.city}</option>`
-
     });
     citySelectorField.innerHTML = cityOptions;
     citySelectorField.disabled = false;
@@ -71,110 +66,114 @@ function addCitiesToList(cities) {
 
 async function getCityData (e) {
     let cityName = e.target.value;
-    let resCity = await fetch(`${baseURL}city?city=${cityName}&state=${selectedStateName}&country=USA&key=${apiKey}`);
-    cityData = await resCity.json();
-    // console.log(cityData);
-    createVisualBar(cityData);
-    // console.log(nearestCityData.data.current.pollution.aqius);
+    if(cityName !== "Choose..."){
+        let resCity = await fetch(`${baseURL}city?city=${cityName}&state=${selectedStateName}&country=USA&key=${apiKey}`);
+        cityData = await resCity.json();
+        // console.log(cityData);
+        createVisualBar(cityData);
+    }
+
 }
 
 //Initialize a function to remove a card when the remove card button is clicked
 function removeCard (e){
-    
     let currentCard = e.target.parentNode;
     let cardsContainer = e.target.parentNode.parentNode;
+    currentCard.style.opacity = '0';
+    setTimeout(function(){cardsContainer.removeChild(currentCard)}, 500)
     console.log("Remove Card Has Been Triggered for " + currentCard);
-    cardsContainer.removeChild(currentCard);
+    // cardsContainer.removeChild(currentCard);
 }
 
 function createVisualBar(n){
+    //Initialize data points array
+    let weatherDataArray = [];
+    function weatherDataObject (abbrev, name, dataPoint, percentString, city, state) {
+        this.abbrev = abbrev;
+        this.name = name;
+        this.dataPoint = dataPoint;
+        this.percentString = percentString;
+        this.city = city;
+        this.state = state;
+    }
 
-        //Initialize data points array
-        let weatherDataArray = [];
-        function weatherDataObject (abbrev, name, dataPoint, percentString, city, state) {
-            this.abbrev = abbrev;
-            this.name = name;
-            this.dataPoint = dataPoint;
-            this.percentString = percentString;
-            this.city = city;
-            this.state = state;
+    //Create the outer card for the city
+    let cityCard = document.createElement('div');
+    cityCard.className = "card p-3 m-4";
+    //Set up properties to fade out when removed
+    cityCard.style.transitionDuration = "499ms";
+    cityCard.style.transitionProperty ="opacity"
+    cityCard.style.opacity="1"
+
+    //Set custom dom attributes for city and state to reference on events
+    cityCard.setAttribute("state-name", "" + n.data.state + "") ;
+    cityCard.setAttribute("city-name", "" + n.data.city + "") ;
+
+    removeCardButtons = document.getElementsByClassName("remove-card")
+
+    //Create remove card button
+    let removeCardButton = document.createElement('div');
+    removeCardButton.className = "btn btn-secondary remove-card" ;
+    removeCardButtons = document.getElementsByClassName("remove-card");
+    
+
+    //Create City/State Title and add to the city card
+    let cityTitle = document.createElement('h2');
+    cityTitle.innerText = `${n.data.city}, ${n.data.state}`;
+
+    //Append card to static dom element
+    mainCardsContainer.prepend(cityCard);
+    cityCard.appendChild(cityTitle);
+    cityCard.appendChild(removeCardButton);
+
+    //Create an onclick trigger for this card
+    removeCardButton.onclick = removeCard;
+
+    //Process AQI Data (Air Quality Index)
+    let aqi = (n.data.current.pollution.aqius).toFixed(1);
+    let aqiPercent = Math.floor((aqi/300) * 100);
+    let aqiPercentString = `${aqiPercent}%`;
+    weatherDataArray.push(new weatherDataObject("aqi", "Air Quality Index", aqi, aqiPercentString));
+
+    //Process Temp Data
+    let tempFar = (((n.data.current.weather.tp) * (9/5)) + 32).toFixed(1) ;
+    let tempFarString = Math.floor(tempFar.toString());
+    let tempFarPercentString= `${tempFar}%`
+
+    weatherDataArray.push(new weatherDataObject("temp", "Temperature (F)", tempFar, tempFarPercentString));
+
+    //Iterate through data and create dom elements
+
+    weatherDataArray.forEach(weatherDataObject => {
+        //Create the data point Title
+        let Title = document.createElement('h3');
+        Title.innerText = `${weatherDataObject.name}: ${weatherDataObject.dataPoint}`;
+
+        //Create the aqi bar container
+        let barContainer = document.createElement('div');
+        barContainer.className = "visual-bar-container";
+
+        //Set Inner Bar color
+        let barColor = "black"
+        if (weatherDataObject.abbrev == "aqi") {
+            if (weatherDataObject.dataPoint < 51) {barColor = "Green"}
+            else if (weatherDataObject.dataPoint < 101) {barColor = "Yellow"}
+        }
+        else if (weatherDataObject.abbrev == "temp") {
+            barColor = "#ffa661"
+
         }
 
-        //Create the outer card for the city
-        let cityCard = document.createElement('div');
-        cityCard.className = "card p-3 m-4";
+        //Create the inner bar
+        let barInner = document.createElement('div');
+        barInner.className = "visual-bar-inside";
+        barInner.style.width = weatherDataObject.percentString;
+        barInner.style.backgroundColor = barColor;
 
-        //Set custom dom attributes for city and state to reference on events
-        cityCard.setAttribute("state-name", "" + n.data.state + "") ;
-        cityCard.setAttribute("city-name", "" + n.data.city + "") ;
+        //Add all the dom elements to the page
+        cityCard.appendChild(Title);
+        cityCard.appendChild(barContainer);
+        barContainer.appendChild(barInner);
 
-        removeCardButtons = document.getElementsByClassName("remove-card")
-
-        //Create remove card button
-        let removeCardButton = document.createElement('div');
-        removeCardButton.className = "btn btn-secondary remove-card" ;
-        removeCardButtons = document.getElementsByClassName("remove-card");
-        
-
-        //Create City/State Title and add to the city card
-        let cityTitle = document.createElement('h2');
-        cityTitle.innerText = `${n.data.city}, ${n.data.state}`;
-
-        //Append card to static dom element
-        mainCardsContainer.prepend(cityCard);
-        cityCard.appendChild(cityTitle);
-        cityCard.appendChild(removeCardButton);
-
-        //Create an onclick trigger for this card
-        removeCardButton.onclick = removeCard;
-
-        //Process AQI Data (Air Quality Index)
-        let aqi = (n.data.current.pollution.aqius).toFixed(1);
-        // let aqi = 51; // for testing, comment out the above line and use this number
-        let aqiPercent = Math.floor((aqi/300) * 100);
-        let aqiPercentString = `${aqiPercent}%`;
-        weatherDataArray.push(new weatherDataObject("aqi", "Air Quality Index", aqi, aqiPercentString));
-
-        //Process Temp Data
-        let tempFar = (((n.data.current.weather.tp) * (9/5)) + 32).toFixed(1) ;
-        let tempFarString = Math.floor(tempFar.toString());
-        let tempFarPercentString= `${tempFar}%`
-
-        weatherDataArray.push(new weatherDataObject("temp", "Temperature (F)", tempFar, tempFarPercentString));
-   
-        //Iterate through data and create dom elements
-
-        weatherDataArray.forEach(weatherDataObject => {
-            //Create the data point Title
-            let Title = document.createElement('h3');
-            Title.innerText = `${weatherDataObject.name}: ${weatherDataObject.dataPoint}`;
-
-            //Create the aqi bar container
-            let barContainer = document.createElement('div');
-            barContainer.className = "visual-bar-container";
-
-            //Set Inner Bar color
-            let barColor = "black"
-            if (weatherDataObject.abbrev == "aqi") {
-                if (weatherDataObject.dataPoint < 51) {barColor = "Green"}
-                else if (weatherDataObject.dataPoint < 101) {barColor = "Yellow"}
-            }
-            else if (weatherDataObject.abbrev == "temp") {
-                barColor = "#ffa661"
-
-            }
-
-            //Create the inner bar
-            let barInner = document.createElement('div');
-            barInner.className = "visual-bar-inside";
-            barInner.style.width = weatherDataObject.percentString;
-            barInner.style.backgroundColor = barColor;
-
-            //Add all the dom elements to the page
-
-            cityCard.appendChild(Title);
-            cityCard.appendChild(barContainer);
-            barContainer.appendChild(barInner);
-
-        });
+    });
 }
